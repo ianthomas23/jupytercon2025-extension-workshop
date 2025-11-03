de508f36055ed38e5091b4f3124333eb0dd3b35d
Ian Thomas <ianthomas23@gmail.com>
2025-11-03 11:34:52 +0000

    Register widget with launcher

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 import { ICommandPalette } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher'
 
 import { requestAPI } from './request';
 import { ImageCaptionMainAreaWidget } from './widget';
@@ -15,10 +16,11 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description:
     'A JupyterLab extension that displays a random image and caption.',
   autoStart: true,
-  requires: [ICommandPalette],  // dependencies of our extension
+  requires: [ICommandPalette, ILauncher],  // dependencies of our extension
   activate: (
     app: JupyterFrontEnd,
-    palette: ICommandPalette
+    palette: ICommandPalette,
+    launcher: ILauncher
   ) => {
     console.log(
       'JupyterLab extension jupytercon2025-extension-workshop is activated!'
@@ -49,6 +51,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     });
 
     palette.addItem({ command: command_id, category: 'Tutorial' });
+    launcher.add({ command: command_id });
   }
 };
 
